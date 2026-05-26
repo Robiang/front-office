7e20025e97ae9203e4689e744ae9c5f0c412fb3c
benoitmondjo <benoitmondjo@gmail.com>
2026-05-26 10:58:57 +0100

    version 1: update

--- src/assets/js/layouts/header.js
+++ src/assets/js/layouts/header.js
@@ -1,12 +1,14 @@
 
 const header = document.querySelector(".header")
-const navbar = document.querySelector(".header .flex .navbar")
+const navMobile = document.querySelector(".header .flex .nav-mobile")
 const menuBtn = document.querySelector("#menu-btn");
+const links = document.querySelectorAll(".header .navbar .link")
+const logo = document.querySelector(".header .flex .logo")
 
 menuBtn.addEventListener('click', ()=>{
     menuBtn.classList.toggle('fa-bars-staggered')
     menuBtn.classList.toggle('fa-xmark')
-    navbar.classList.toggle('active')
+    navMobile.classList.toggle('active')
     
 })
 
@@ -14,13 +16,19 @@ menuBtn.addEventListener('click', ()=>{
 window.addEventListener('scroll', ()=>{
     menuBtn.classList.remove('fa-xmark')
     menuBtn.classList.add('fa-bars-staggered')
-    navbar.classList.remove('active')
-
-
+    navMobile.classList.remove('active')
+    
     if(window.scrollY > 20 ){
         header.classList.add('active')
+        logo.classList.add('active')
+        menuBtn.classList.add('active')
+        links.forEach(l => l.classList.add("active"))
     }
     else{
         header.classList.remove('active')
+        logo.classList.remove('active')
+        menuBtn.classList.remove('active')
+        links.forEach(l => l.classList.remove("active"))
+
     }
 })
